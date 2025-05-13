25296d5cea2c43c20f94db2bd4944738a232e8b5
Mr-Pine <git@mr-pine.de>
2025-05-13 17:29:31 +0200

    Make NamedLocalFile clonable

--- src/local_file.rs
+++ src/local_file.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 /// A file on the local file system and the name it should have when uploaded
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct NamedLocalFile {
     pub name: String,
     pub path: PathBuf,
